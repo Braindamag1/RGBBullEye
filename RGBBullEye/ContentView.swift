259427b839f4e9ff58b2a7e18d526771f1dab66f
YJ.Li <yuanjun.li@beyond-power.com>
2022-06-03 19:52:38 +0800

    Initial Commit

---
+++ RGBBullEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBBullEye
+//
+//  Created by YJ.Lee on 2022/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RGBBullEye/RGBBullEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBBullEyeApp.swift
+//  RGBBullEye
+//
+//  Created by YJ.Lee on 2022/6/3.
+//
+
+import SwiftUI
+
+@main
+struct RGBBullEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
